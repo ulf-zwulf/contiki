d84a8910f736ab9f99a4345f410c47d9dc57f5d7
Robert Olsson <robert@herjulf.se>
2016-09-01 16:34:21 +0200

    Fixed flash script

--- platform/avr-rss2/apps/sensd-rime/flash.sh
+++ platform/avr-rss2/apps/sensd-rime/flash.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-make bc hex TARGET=avr-RSS2
+make TARGET=avr-RSS2
 
-S_PROG=avr109
+#S_PROG=avr109
 S_PROG=stk500v2
 
-PGM=
+
+PGM=sensd_rime.avr-rss2
 
 stty sane
 tty_talk -38400 /dev/ttyUSB0 "upgr
 "
-avrdude -p m128rfa1 -c $S_PROG -P /dev/ttyUSB0 -b 38400 -e -U flash:w:bc.hex
-avrdude -p m256rfr2 -c $S_PROG -P /dev/ttyUSB0 -b 38400 -e -U flash:w:bc.hex
+#avrdude -p m128rfa1 -c $S_PROG -P /dev/ttyUSB0 -b 38400 -e -U flash:w:$PGM
+avrdude -p m256rfr2 -c $S_PROG -P /dev/ttyUSB0 -b 38400 -e -U flash:w:$PGM
