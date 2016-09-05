de3f48bcbae3f2e06006343b0504dd09b2e6c58c
Ulf Kulau <kulau@ibr.cs.tu-bs.de>
2016-09-05 16:02:32 +0200

    header fix

--- platform/avr-rss2/apps/pdr-test/pdr.h
+++ platform/avr-rss2/apps/pdr-test/pdr.h
@@ -21,7 +21,7 @@
 // test ID (useful e.g. in case of multiple motes having simultaneous tests)
 #define TEST_ID 0x1
 
-#define STAT_SIZE                   50
+#define STAT_SIZE                   100
 #define TEST_PACKET_SIZE            (6 + 6 * 10)
 #define PACKETS_IN_TEST             100u
 
